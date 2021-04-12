32359c34d4c05f5e72a4fa53bc61dd1303abde54
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-04-12 22:33:29 +0300

    simple test of presenting me in the Internet

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# For detail instructions look here: 👀 https://sites.google.com/a/chromium.org/chromedriver/getting-started
+# and here https://selenium-python.readthedocs.io/getting-started.html
+
+# TODO:  before run of this project download right chrome driver
+#  👉 https://sites.google.com/a/chromium.org/chromedriver/downloads
 driver = webdriver.Chrome('./chromedriver')
-driver.get("http://www.python.org")
-assert "Python" in driver.title
+
+driver.get("http://www.google.com")
+assert "Google" in driver.title
 elem = driver.find_element_by_name("q")
 elem.clear()
-elem.send_keys("pycon")
+elem.send_keys("d9nich")
+time.sleep(5)  # Let the user actually see something!
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+assert "Danylo Halaiko" in driver.page_source
+elem = driver.find_element_by_css_selector('a[href="https://github.com/d9nchik"]')
+elem.click()
+assert 'Ask me about Java, Python, Golang, JavaScript, Kotlin, TypeScript, C++' in driver.page_source
+time.sleep(5)  # Let the user actually see something!
 driver.close()
