5b0d94948d7a00555cd7a05f1e612b70dec913bf
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-04-23 18:30:22 +0300

    add scenario outline

--- features/steps/simple.py
+++ features/steps/simple.py
@@ -2,37 +2,37 @@ from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-use_step_matcher("re")
 
-driver = webdriver.Chrome()
-
+# More about scenario outline 👀 here https://jenisys.github.io/behave.example/tutorials/tutorial04.html
 
 @given("I have access to the internet")
 def step_impl(context):
+    context.driver = webdriver.Chrome()
     """
     :type context: behave.runner.Context
     """
-    driver.get("http://www.google.com")
-    assert "Google" in driver.title
+    context.driver.get("https://www.google.com")
+    assert "Google" in context.driver.title
 
 
-@step("I have entered d9nich in google search")
-def step_impl(context):
+@step('I have entered {nickname} in google search')
+def step_impl(context, nickname):
     """
     :type context: behave.runner.Context
     """
-    elem = driver.find_element_by_name("q")
+    assert nickname != ''
+    elem = context.driver.find_element_by_name("q")
     elem.clear()
-    elem.send_keys("d9nich")
+    elem.send_keys(nickname)
     elem.send_keys(Keys.RETURN)
 
 
-@step('page contains "Danylo Halaiko"')
-def step_impl(context):
+@step('page contains {realName}')
+def step_impl(context, realName):
     """
     :type context: behave.runner.Context
     """
-    assert "Danylo Halaiko" in driver.page_source
+    assert realName in context.driver.page_source
 
 
 @when("I click on first github link")
@@ -40,15 +40,15 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    elem = driver.find_element_by_css_selector(
-        'a[href="https://github.com/d9nchik"]')
+    elem = context.driver.find_element_by_css_selector(
+        'a[href^="https://github.com"]')
     elem.click()
 
 
-@then("it would be d9nich github main page")
-def step_impl(context):
+@then('it would contain {learningSkills}')
+def step_impl(context, learningSkills):
     """
     :type context: behave.runner.Context
     """
-    assert 'Ask me about Java, Python, Golang, JavaScript, Kotlin, TypeScript, C++' in driver.page_source
-    driver.close()
+    assert learningSkills in context.driver.page_source
+    context.driver.close()
