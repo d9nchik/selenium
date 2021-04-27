e02d51d1de1695fec1267e0d26372c5673bea1b3
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-04-27 14:37:35 +0300

    extract common part of GithubPO.py and GooglePO.py to DefaultPO.py

--- src/po/GithubPO.py
+++ src/po/GithubPO.py
@@ -1,8 +1,5 @@
-class GithubPO:
+import src.po.DefaultPO as dpo
 
-    def __init__(self, driver) -> None:
-        super().__init__()
-        self.driver = driver
 
-    def get_page_source(self):
-        return self.driver.page_source
+class GithubPO(dpo.DefaultPO):
+    pass
